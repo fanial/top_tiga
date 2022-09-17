bb71904ccf246634e117db5e552754b11a168834
Fani Aghistiya Lestari <fani.aghistiya.lestr@gmail.com>
2022-09-17 21:33:44 +0700

    hapus file yang tidak digunakan

--- app/src/main/java/com/fal/top_tiga/latihansatu/DataProduk.kt
+++
@@ -1,9 +0,0 @@
-package com.fal.top_tiga.latihansatu
-
-data class DataProduk(
-    val nm_produk : String,
-    val harga : String,
-    val stok : Int,
-    val desc : String,
-    val img_produk : Int) {
-}
